6af6de9a13dd88ca336711b6f8b73108c6a4dee6
Arthur Fait <afait@luxoft.com>
2015-10-18 13:59:58 +0300

    add about text

--- src/audacious/plugin/plugin_entry.c
+++ src/audacious/plugin/plugin_entry.c
@@ -14,6 +14,7 @@ AUD_GENERAL_PLUGIN
 (
    .name = "SoundCloud",
    .domain = "soundcloud",
+   .about_text = "SoundCloud plugin 0.2 by konqwenqor. \n\nCopyright © 2014-2015 Arthur Fait <konqwenqor@gmail.com>.\n\nFor search by tag put '# in front of query.\nFor search by genre put '@' in front of query.",
    .init = plugin_init,
    .cleanup = plugin_cleanup,
    .get_widget = plugin_get_widget
